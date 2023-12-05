ba336c8ca1e909268bad6d23f8dae4fe7cd1a50d
Gecx <gechangxing@qijian-tech.com>
2023-12-05 14:29:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+### 注意接口 
+/api/login  返回JSON格式结果
+/login 返回渲染的html
---
+++ config/config.js
@@ -0,0 +1,9 @@
+//配置文件
+module.exports = {
+  DBHOST: '127.0.0.1',
+  DBPORT: 27017,
+  DBNAME: 'bilibili',
+  USERNAME: 'hello',
+  PASSWORD: '123456',
+  secret: 'hellworld'
+}
